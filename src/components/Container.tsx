cd206356994a470084bf68edc9178ddb55e6f9ce
Glenn-Rhee <glennviktor5@gmail.com>
2026-06-11 12:49:37 +0700

    feat: added navbar component

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -7,7 +7,7 @@ interface Props {
 export default function Container(props: Props) {
   const { children, className } = props;
   return (
-    <div className={cn("w-full py-2 md:py-4 px-3 md:px-6", className)}>
+    <div className={cn("w-full py-2 md:py-4 px-4 md:px-8", className)}>
       {children}
     </div>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,44 @@
+import Link from "next/link";
+import Container from "./Container";
+
+const menuNavbar = [
+  {
+    title: "Home",
+    href: "/",
+  },
+  {
+    title: "Tentang Program",
+    href: "#",
+  },
+  {
+    title: "Bergabung",
+    href: "#",
+  },
+];
+
 export default function Navbar() {
   return (
-    <>
-      <nav className="hidden md:flex items-center justify-between"></nav>
-    </>
+    <nav className="hidden md:block shadow-md sticky top-0 right-0 left-0 py-3">
+      <Container className="md:flex items-center justify-between">
+        <Link href={"/"} className="font-bold text-3xl">
+          Mahreen Indonesia.
+        </Link>
+        <ul className="flex justify-around items-center gap-x-16">
+          {menuNavbar.map((menu) => (
+            <li
+              key={menu.href + menu.title}
+              className="font-medium text-xl hover:underline"
+            >
+              <Link href={menu.href}>{menu.title}</Link>
+            </li>
+          ))}
+          <li className="font-medium text-xl hover:underline">
+            <button className="bg-primary px-5 py-2 rounded-xl cursor-pointer active:scale-95 hover:bg-[#FFD52E] transition-all duration-200">
+              Daftar Sekarang
+            </button>
+          </li>
+        </ul>
+      </Container>
+    </nav>
   );
 }
